728cb90b7616e55019518bc1687db2d3ab507734
daoduyquy321323 <daoduyquylop97@gmail.com>
2024-12-02 02:00:16 +0700

    deploy

--- src/components/api/index.js
+++ src/components/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: 'https://print-system-production.up.railway.app/',
+    baseURL: `${process.env.REACT_APP_BACKEND_URL}/`,
     withCredentials: true,
 })
 
