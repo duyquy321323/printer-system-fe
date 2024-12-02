becb4529b8d91251a4e62cb8bfa08d09cd9940c2
daoduyquy321323 <daoduyquylop97@gmail.com>
2024-12-02 10:48:43 +0700

    deplooy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from 'react-redux';
@@ -6,10 +8,26 @@ import App from "./App";
 import "./index.css";
 import store from "./redux/store";
 
+const theme = createTheme({
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          backgroundColor: 'red',
+          '&:hover': {
+            backgroundColor: 'darkred',
+          },
+        },
+      },
+    },
+  },
+});
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
+    <ThemeProvider theme={theme}>
       <App />
+  </ThemeProvider>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
